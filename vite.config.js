b73fd24ab1c847ab58eaf682e903c707f848fafb
Dias Sagatov <101350324+diassagatov@users.noreply.github.com>
2024-05-06 15:13:36 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { swcPlugin } from 'vite-plugin-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
