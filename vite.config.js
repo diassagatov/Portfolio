a72cb79a2899b2710559d079dde4d5d808af8e38
Dias Sagatov <101350324+diassagatov@users.noreply.github.com>
2024-05-06 15:09:00 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
+import { swcPlugin } from 'vite-plugin-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), swcPlugin()],
   base: "/portfolio/"
 })
